944e7e59794afdcb2784086d612012c865f6de57
Harald Welte <laforge@osmocom.org>
2023-06-08 13:57:36 +0200

    IPSP: don't route any incoming M3UA messages in IPSP case
    An IPSP is a point-to-point association, and we must not route any
    such messages.
    
    Change-Id: Iad7280619ec5814cda7a179418079048a5955976

--- src/m3ua.c
+++ src/m3ua.c
@@ -41,6 +41,7 @@
 #include <osmocom/sigtran/protocol/sua.h>
 
 #include "mtp3_hmdc.h"
+#include "mtp3_hmdt.h"
 #include "xua_as_fsm.h"
 #include "xua_asp_fsm.h"
 #include "xua_internal.h"
@@ -667,8 +668,14 @@ static int m3ua_rx_xfer(struct osmo_ss7_asp *asp, struct xua_msg *xua)
 		xua_msg_free_tag(xua, M3UA_IEI_ROUTE_CTX);
 	}
 
+	/* an IPSP by definition is a peer-to-peer service that doesn't
+	 * use a signaling gateway, and hence doesn't route messages.
+	 * See RFC 4666 Section 1.4.3.4. */
 	/* xua ownership is passed here: */
-	return mtp3_hmdc_rx_from_l2(asp->inst, xua);
+	if (asp->cfg.role == OSMO_SS7_ASP_ROLE_IPSP)
+		return mtp3_hmdt_message_for_distribution(asp->inst, xua);
+	else
+		return mtp3_hmdc_rx_from_l2(asp->inst, xua);
 
 ret_free:
 	xua_msg_free(xua);
