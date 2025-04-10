osmocom/libosmo-sccp

commit 2c9ba16c2c0210a189c72064eafad5ef336254cd

ipa: Use pseudo-random number for SLS in IPA->M3UA direction
In Change-Id Ice7bab997b84cfed00c7d6d780c70f4e9fac6002 we introduced
code that would make the LSB of the file descriptor be used as SLS
when passing packets from IPA in M3UA direction.

This did however not achieve sufficient entropy in real-world use cases.

In this change, we change over to allocating a pseudo-random SLS to each
IPA connection at the time it is established;  We then assign that SLS
to each packet received on that IPA connection.

Change-Id: Ia4e66d660b6057338f66a47fffc8a0d32759f733
Related: SYS#6543
Closes: SYS#6802

src/osmo_ss7_asp.c
@@ -803,9 +803,9 @@ int ss7_asp_ipa_srv_conn_rx_cb(struct osmo_stream_srv *conn, struct msgb *msg)
 
 	msg->dst = asp;
 	rate_ctr_inc2(asp->ctrg, SS7_ASP_CTR_PKT_RX_TOTAL);
-	/* we can use the 'fd' return value of osmo_stream_srv_get_fd() here unverified as all we do
-	 * is 'roll the dice' to obtain a 4-bit SLS value. */
-	return ipa_rx_msg(asp, msg, fd & 0xf);
+	/* we simply use the lower 4 bits of the asp_id, which is initialized to a pseudo-random value upon
+	 * connect */
+	return ipa_rx_msg(asp, msg, asp->asp_id & 0xf);
 }
 
 /* netif code tells us we can read something from the socket */
@@ -916,6 +916,12 @@ static int xua_cli_connect_cb(struct osmo_stream_cli *cli)
 	if (asp->cfg.trans_proto == IPPROTO_SCTP) {
 		rc = ss7_asp_apply_peer_primary_address(asp);
 		rc = ss7_asp_apply_primary_address(asp);
+	} else {
+		if (asp->cfg.proto == OSMO_SS7_ASP_PROT_IPA) {
+			/* we use the lower 4 bits of the asp_id feld as SLS; let's initialize it here from a
+			 * pseudo-random value */
+			asp->asp_id = rand() & 0xf;
+		}
 	}
 
 	if (asp->lm && asp->lm->prim_cb) {

stp/stp_main.c
@@ -213,6 +213,8 @@ int main(int argc, char **argv)
 {
 	int rc;
 
+	srand(time(NULL));
+
 	tall_stp_ctx = talloc_named_const(NULL, 1, "osmo-stp");
 	msgb_talloc_ctx_init(tall_stp_ctx, 0);
 	osmo_init_logging2(tall_stp_ctx, &log_info);

osmocom/libosmo-sigtran

commit ed998c884fec743c603c08844faa75169ae2a292

ipa: Use pseudo-random number for SLS in TCP-client too
Previous commit (see below) replaced the SLS generation from using
the LSB of the file descriptor to a pseudo-random SLS since the former
didn't really achieve enough entropy in real world use cases.

However, that commit only applied it for TCP-server conns, but forgot to
update TCP-client ones.
Present commit fixes that missing part. asp->asp_id is already being set
using a pseudo-randome number in xua_cli_connect_cb().

Fixes: 2c9ba16c2c0210a189c72064eafad5ef336254cd
Related: SYS#6543
Related: SYS#6802
Change-Id: I837505868b608ac5ed658a7e98bb3eeebe3e852c

src/osmo_ss7_asp.c
@@ -1027,7 +1027,6 @@ static int xua_cli_disconnect_cb(struct osmo_stream_cli *cli)
 /* read call-back for IPA/SCCPlite socket */
 static int ipa_cli_read_cb(struct osmo_stream_cli *conn, int res, struct msgb *msg)
 {
-	int fd = osmo_stream_cli_get_fd(conn);
 	struct osmo_ss7_asp *asp = osmo_stream_cli_get_data(conn);
 
 	if (res <= 0) {
@@ -1042,9 +1041,9 @@ static int ipa_cli_read_cb(struct osmo_stream_cli *conn, int res, struct msgb *m
 
 	msg->dst = asp;
 	rate_ctr_inc2(asp->ctrg, SS7_ASP_CTR_PKT_RX_TOTAL);
-	/* we can use the 'fd' return value of osmo_stream_srv_get_fd() here unverified as all we do
-	 * is 'roll the dice' to obtain a 4-bit SLS value. */
-	return ipa_rx_msg(asp, msg, fd & 0xf);
+	/* we simply use the lower 4 bits of the asp_id, which is initialized to a pseudo-random value upon
+	 * connect */
+	return ipa_rx_msg(asp, msg, asp->asp_id & 0xf);
 }
 
 /* read call-back for M3UA-over-TCP socket */